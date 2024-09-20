04c866d54b83ad67ed159127fa7f68cdcf3b3b13
arthur Windows <rybalkin.arthur@gmail.com>
2024-09-20 13:08:12 +0200

    aufhuebschen

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,54 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Simple Weather App"),
-        ),
-        body: Container(
-          child: const Text(
-            'Willkommen zur Wetter-App!',
-            style: TextStyle(color: Colors.blue, fontSize: 24),
+          appBar: AppBar(
+            title: const Text(
+              'Simple Weater App',
+              style: TextStyle(
+                  color: Colors.blue, fontSize: 24, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+            ),
           ),
+          body: const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [weatherWidget(city: "Berlin", temperature: 21, weather: "Regnerisch")],
+          )),
+    );
+  }
+}
+
+class weatherWidget extends StatelessWidget {
+  final String city;
+  final double temperature;
+  final String weather;
+  const weatherWidget({super.key, required this.city, required this.temperature, required this.weather});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Stadt: ",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            Text(city)
+          ],
         ),
-      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Temperatur: ", style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(temperature.toString())
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("Wetter: ", style: TextStyle(fontWeight: FontWeight.bold)), Text(weather)],
+        )
+      ],
     );
   }
 }
